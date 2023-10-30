87c18897ffbe5d0b967889233d10bd331f37d0e7
Enxo Rodrigues <enzo.orodrigues03@gmail.com>
2023-10-30 14:58:32 -0300

    Aula: 21-08-23

---
+++ 1Sesmestre/AULA_21_08_2023/21-08-23.py
@@ -0,0 +1,117 @@
+# #print(type('Hello World'))
+# #frase = 'Hello World'
+# #frase = 'Hello' + " " + "World"
+# #print(frase)
+# #operaÃ§Ãµes : + - * / % //
+
+# '''
+# num1 = int(input("Diga um numero : "))
+# num2 = int(input("Diga um numero : "))
+# print(f"A soma entre {num1} e {num2} Ã© {num1 + num2}")
+# print(f"A subtraÃ§Ã£o entre {num1} e {num2} Ã© {num1 - num2}")
+# print(f"A multiplicaÃ§Ã£o entre {num1} e {num2} Ã© {num1*num2}")
+# print(f"A divisÃ£o entre {num1} e {num2} Ã© {num1/num2}")
+# print(f"O resto na divisÃ£o de {num1} por {num2} Ã© {num1%num2}")
+# print(f"A parte inteira da divisÃ£o de {num1} por {num2} Ã© {num1//num2}")
+# '''
+
+# '''a = "Danilo"
+# b = "JÃ³"
+# print(a,b)
+# a, b = b, a
+# aux = a
+# a = b
+# b = aux
+# print(a,b)
+
+# frase = "Hello"
+# print(frase)
+# frase = frase + " World"
+# print(frase)
+
+# contador = 1
+# frase = ''
+# palavra = input(f"{contador}Âª palavra : ")
+# frase = frase + " " + palavra
+# contador = contador + 1
+# print(frase)
+# palavra = input(f"{contador}Âª palavra : ")
+# frase = frase + " " + palavra
+# contador = contador + 1
+# print(frase)
+# palavra = input(f"{contador}Âª palavra : ")
+# frase = frase + " " + palavra
+# contador = contador + 1
+# print(frase)
+# palavra = input(f"{contador}Âª palavra : ")
+# frase = frase + " " + palavra
+# print(frase)
+
+
+# var1 = 2<3
+# var2 = False
+# print(var1)
+# print(type(var2))
+# print(f"A comparaÃ§Ã£o {num1} > {num2} resulta em {num1 > num2}")
+# print(f"A comparaÃ§Ã£o {num1} >= {num2} resulta em {num1 >= num2}")
+# print(f"A comparaÃ§Ã£o {num1} < {num2} resulta em {num1 < num2}")
+# print(f"A comparaÃ§Ã£o {num1} <= {num2} resulta em {num1 <= num2}")
+# print(f"A comparaÃ§Ã£o {num1} == {num2} resulta em {num1 == num2}")
+# print(f"A comparaÃ§Ã£o {num1} != {num2} resulta em {num1 != num2}")
+
+
+# contador = 1
+# num1 = int(input(f'diga o {contador}Âº numero : '))
+# contador = contador + 1
+# num2 = int(input(f'diga o {contador}Âº numero : '))
+# contador = 1
+# num3 = int(input(f'diga o {contador}Âº numero : '))
+# contador = contador + 1
+# num4 = int(input(f'diga o {contador}Âº numero : '))
+# print(f"{num1}>{num2} or {num3}<{num4}, ou seja, {num1>num2} or {num3<num4} resulta em "
+#       f"{num1>num2 or num3<num4}")
+# print(f"{num1}<{num2} or {num3}>{num4}, ou seja, {num1<num2} or {num3>num4} resulta em "
+#       f"{num1>num2 or num3<num4}")
+# print(f"{num1}>{num2} or {num3}>{num4}, ou seja, {num1>num2} or {num3>num4} resulta em "
+#       f"{num1>num2 or num3>num4}")
+# print(f"{num1}<{num2} or {num3}<{num4}, ou seja, {num1<num2} or {num3<num4} resulta em "
+#       f"{num1<num2 or num3<num4}")
+# print("ComparaÃ§Ãµs com and : ")
+# print(f"{num1}>{num2} and {num3}<{num4}, ou seja, {num1>num2} and {num3<num4} resulta em "
+#       f"{num1>num2 and num3<num4}")
+# print(f"{num1}<{num2} and {num3}>{num4}, ou seja, {num1<num2} and {num3>num4} resulta em "
+#       f"{num1>num2 and num3<num4}")
+# print(f"{num1}>{num2} and {num3}>{num4}, ou seja, {num1>num2} and {num3>num4} resulta em "
+#       f"{num1>num2 and num3>num4}")
+# print(f"{num1}<{num2} and {num3}<{num4}, ou seja, {num1<num2} and {num3<num4} resulta em "
+#       f"{num1<num2 and num3<num4}")
+
+# nota = 6
+# if nota >=5:
+#     print('aprovado')
+# else:
+#     print('reprovado')
+
+
+
+# deficiente = input("Voce tem alguma deficiencia ? ")
+# if idoso=="sim" or deficiente=='sim':
+#     print('pode estacionar')
+# else:
+#     print('estaciona em outro lugar')
+# '''
+
+# idoso = input("VocÃª Ã© idoso ? ")
+# cartao = input("Voce tem cartao de idoso ? ")
+# if idoso == 'sim' and cartao == 'sim':
+#     print('pode estacionar')
+# else:
+#     print('sai pra la')
+
+# nota = 7
+# if nota >= 6 and nota<=10:
+#     print('aprovado')
+# elif nota >=4:
+#     print('exame')
+# else:
+#     print('reprovado')
