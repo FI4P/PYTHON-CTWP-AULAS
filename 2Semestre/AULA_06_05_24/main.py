f8705a219d76d7d0f484b28b633a0e7e9d568c0a
devenzo <enzo.orodrigues03@gmail.com>
2024-05-06 20:29:04 -0300

    Feat: Bubble sort

--- 2Semestre/AULA_06_05_24/main.py
+++ 2Semestre/AULA_06_05_24/main.py
@@ -14,28 +14,51 @@ arraySort = [7 , 5 , 4 , 1 , 3 , 2 ,6]
 
 # print(arraySorted)
 
-def findMenor(lista):
-    menor_indice = 0
-    for number in range(len(lista)):
-       if(lista[number] < lista[menor_indice]):
-           menor_indice = number
-    return menor_indice
+# def findMenor(lista):
+#     menor_indice = 0
+#     for number in range(len(lista)):
+#        if(lista[number] < lista[menor_indice]):
+#            menor_indice = number
+#     return menor_indice
     
 
 
-# bad way
+# # bad way
 
-def selectionSort(lista):
-    for i in range(len(lista)):
-        delayIndex = findMenor(lista[i:])
-        realIndex  = delayIndex + i
-        aux = lista[realIndex]
-        lista[realIndex] = lista[i]
-        lista[i] = aux
+# def selectionSort(lista):
+#     for i in range(len(lista)):
+#         delayIndex = findMenor(lista[i:])
+#         realIndex  = delayIndex + i
+#         aux = lista[realIndex]
+#         lista[realIndex] = lista[i]
+#         lista[i] = aux
+#     return lista
+
+# newLista = selectionSort(arraySort)
+# print(newLista)
+
+
+#
+
+def bubbleSort(lista):
+    j = 0
+    while True:
+        trocas = 0
+        for i in range(len(lista) - 1 ):
+            if(lista[i] > lista[i + 1]):
+                aux = lista[i]
+                lista[i] = lista[i+1]
+                lista[i+1] = aux
+                trocas += 1
+                print(lista)
+        print()
+        j +=1
+        if(trocas == 0):
+            break
     return lista
 
-newLista = selectionSort(arraySort)
-print(newLista)
+bubbleSort(arraySort)
 
 
-# 
+
+
