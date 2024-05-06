af81f6bd75d1b5027eeb262b4fcf2c8cba56bf8b
devenzo <enzo.orodrigues03@gmail.com>
2024-05-06 20:02:36 -0300

    Feat: Bad way to solve the sort

--- 2Semestre/AULA_06_05_24/main.py
+++ 2Semestre/AULA_06_05_24/main.py
@@ -1,14 +1,41 @@
+
+##Worst way
 arraySort = [7 , 5 , 4 , 1 , 3 , 2 ,6]
 
-arraySorted = []
+# arraySorted = []
 
-while arraySort:
+# while arraySort:
+#     menor_indice = 0
+#     for number in range(len(arraySort)):
+#        if(arraySort[number] < arraySort[menor_indice]):
+#            menor_indice = number
+#     numberMenor = arraySort.pop(menor_indice)
+#     arraySorted.append(numberMenor)
+
+# print(arraySorted)
+
+def findMenor(lista):
     menor_indice = 0
-    for number in range(len(arraySort)):
-       if(arraySort[number] < arraySort[menor_indice]):
+    for number in range(len(lista)):
+       if(lista[number] < lista[menor_indice]):
            menor_indice = number
-    numberMenor = arraySort.pop(menor_indice)
-    arraySorted.append(numberMenor)
+    return menor_indice
+    
 
-print(arraySorted)
 
+# bad way
+
+def selectionSort(lista):
+    for i in range(len(lista)):
+        delayIndex = findMenor(lista[i:])
+        realIndex  = delayIndex + i
+        aux = lista[realIndex]
+        lista[realIndex] = lista[i]
+        lista[i] = aux
+    return lista
+
+newLista = selectionSort(arraySort)
+print(newLista)
+
+
+# 
